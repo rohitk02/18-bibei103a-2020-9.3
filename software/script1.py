18106b8faae7bf78250ad524faecb2fab3453118
Rohit Kantipudi <rohitk@caltech.edu>
2020-12-09 20:39:40 -0800

    wow

--- software/script1.py
+++ software/script1.py
@@ -1,2 +1,22 @@
-for i in range(10):
-    print("This is a dummy script.")
+# Colab setup ------------------
+import os, sys, subprocess
+if "google.colab" in sys.modules:
+    cmd = "pip install --upgrade xarray arviz cmdstanpy bebi103 watermark"
+    process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    data_path = "https://s3.amazonaws.com/bebi103.caltech.edu/data/"
+else:
+    data_path = "../data/"
+# ------------------------------
+
+import numpy as np
+import pandas as pd
+
+import holoviews as hv
+hv.extension('bokeh')
+
+import bebi103
+bebi103.hv.set_defaults()
+
+import bokeh.io
+bokeh.io.output_notebook()
